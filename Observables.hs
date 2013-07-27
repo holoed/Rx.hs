ca4fd348145da60d793b99d94458948bdad771e7
holoed <edmondo@gmail.com>
2013-07-27 01:08:18 +0100

    Update Observables.hs

--- Observables.hs
+++ Observables.hs
@@ -34,35 +34,32 @@ merge :: Observable (Observable a) -> Observable a
 merge xss = Observable (\o -> xss |> subscribe (Observer (\xs -> xs |> subscribe o)))
 
 instance Functor Observable where
-    --fmap :: (a -> b) -> Observable a -> Observable b
+    fmap :: (a -> b) -> Observable a -> Observable b
     fmap f m = Observable (\o -> m |> subscribe (Observer (\x -> o |> onNext (f x)))) 
 
 instance Monad Observable where
-    --(>>=) :: Observable a -> (a -> Observable b) -> Observable b
+    (>>=) :: Observable a -> (a -> Observable b) -> Observable b
     m >>= f = merge (fmap f m)
-    --return :: a -> Observable a
+    return :: a -> Observable a
     return = unit 
 
 combine :: Observable a -> Observable b -> Observable (Either a b)
 combine xs ys = Observable (\o -> do xs |> subscribe(Observer (\x -> o |> onNext (Left x)));
- 									 ys |> subscribe(Observer (\x -> o |> onNext (Right x))))
+ 			             ys |> subscribe(Observer (\x -> o |> onNext (Right x))))
 
 instance MonadPlus Observable where
-	--mzero :: Observable a
+	mzero :: Observable a
 	mzero = Observable (\o -> return ())
-	--mplus :: Observable a -> Observable a -> Observable a
+	mplus :: Observable a -> Observable a -> Observable a
 	mplus xs ys = error "Not implemented yet"
-
 
 takeWhile :: (a -> Bool) -> Observable a -> Observable a
 takeWhile p xs = do x <- xs;
-					guard (p x);
-					return x
-
+		    guard (p x);
+		    return x
 
 takeUntil :: Observable a -> Observable b -> Observable a
 takeUntil xs sig = combine xs sig |> takeWhile isLeft |> map (\(Left x) -> x) 
-
 
 toObservable :: [a] -> Observable a
 toObservable xs = Observable (\o -> (mapM_ (\ x -> o |> onNext x) xs))
